ced2a6e95562f51281e803a941bd9165b3160fb5
RavenBeastBoy <113969916+C109118214@users.noreply.github.com>
2025-03-26 19:46:27 +0800

    Update train_deepspeed.py

--- scirpts/distributed/train_deepspeed.py
+++ scirpts/distributed/train_deepspeed.py
@@ -1,1 +1,17 @@
+from transformers import Trainer, TrainingArguments
+import deepspeed
+
+training_args = TrainingArguments(
+    output_dir="./models_deepspeed",
+    per_device_train_batch_size=4,
+    deepspeed="./ds_config.json"
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=None
+)
+
+trainer.train()
 
