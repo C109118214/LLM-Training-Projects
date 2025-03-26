236394240312ddaba526699a279dcbd6dcd22c46
RavenBeastBoy <113969916+C109118214@users.noreply.github.com>
2025-03-26 19:45:29 +0800

    Update deploy_api.py

--- scirpts/deploy_api.py
+++ scirpts/deploy_api.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI
+from transformers import pipeline
 
+app = FastAPI()
+generator = pipeline("text-generation", model="./models_finetune")
+
+@app.get("/generate")
+def generate(text: str):
+    return generator(text, max_length=100)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
